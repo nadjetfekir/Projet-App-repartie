1dfef52a1c802f8613521644972136bc40cbec5f
unknown <gn_fekir@esi.dz>
2021-12-06 16:09:16 +0100

    formation

---
+++ src/formation/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from multiselectfield import MultiSelectField
+
+# Create your models here.
+from django.db import models
+class Formation(models.Model):
+    nomFormation = models.CharField(max_length=255, blank=True)
+    sujet = models.TextField()
+    date = models.DateTimeField()
+    pourcentageEngagement = models.FloatField()
+    pourcentageSatisfaction = models.FloatField()
+    MotCleFormateur = models.CharField(max_length=255)
+    MotClePersonnel = models.CharField(max_length=255)
+   
+    
--- src/mainproject/settings.py
+++ src/mainproject/settings.py
@@ -26,8 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     #our apps
-    'personnel'
+    'personnel',
+    'formation',
 
 ]
 
