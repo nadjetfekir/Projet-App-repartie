ab7fb024107d7d2525519c3cd6ed69d1e75f8f79
alao1908 <gm_douzi@esi.dz>
2021-12-06 17:27:57 +0100

    added zone

--- src/mainproject/settings.py
+++ src/mainproject/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'personnel',
     'operationCommerciale',
     'formation',
+    'zone',
 
 ]
 
---
+++ src/zone/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Zone
+# Register your models here.
+admin.site.register(Zone)
